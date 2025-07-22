f7f7694bcd09f63afc6b2a3419d761e7bcf0417a
gabrimachado147 <gabrielenigmalabs@gmail.com>
2025-07-22 01:16:50 -0300

    fix: garante WagmiProvider no main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
+import { WagmiProvider, createConfig, http } from 'wagmi'
+import { base } from 'wagmi/chains'
+import { injected } from 'wagmi/connectors'
+
+const config = createConfig({
+  chains: [base],
+  transports: {
+    [base.id]: http(import.meta.env.VITE_RPC_URL),
+  },
+  connectors: [injected()],
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <WagmiProvider config={config}>
+      <App />
+    </WagmiProvider>
+  </React.StrictMode>
 )
 
